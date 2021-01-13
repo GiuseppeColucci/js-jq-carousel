f4229ec1b8f5fa3840240afbc3798cc295dcd95c
giuseppecolucci <giuseppecolucciwork@gmail.com>
2021-01-13 17:17:14 +0100

    fatto manca il bonus

--- main.js
+++ main.js
@@ -1,14 +1,36 @@
 $(document).ready(function(){
-//button sinista
+    
+    //button sinista
     $('.prev').click(function(){
-        $('.active').removeClass('active').prev().addClass('active');
+        var images2=$('.images img.active');
+        images2.removeClass('active');
+        if(images2.hasClass('last')){
+            var ultimaImg=$('.images img.first');
+            ultimaImg.addClass('active')
+        }
+        else{
+            images2.next().addClass('active');
 
+        }
+        
     })
-
-//button destra
-
+    
+    
+    
+    //button destra
+    
     $('.next').click(function(){
-        $('.active').removeClass('active').next().addClass('active');
+        var images=$('.images img.active');
+        images.removeClass('active');
+
+        if(images.hasClass('last')){
+            var primaImg=$('.images img.first');
+            primaImg.addClass('active')
+        }
+        else{
+            images.next().addClass('active');
+
+        }
     })
 
 
